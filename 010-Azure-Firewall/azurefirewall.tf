79846b91f4fbcfbb91036480a9c0952b19e83c47
Sathyanarayanan-SK <135942886+Sathyanarayanan-SK@users.noreply.github.com>
2024-05-13 11:49:06 +0530

    Update azurefirewall.tf

--- 010-Azure-Firewall/azurefirewall.tf
+++ 010-Azure-Firewall/azurefirewall.tf
@@ -1,35 +1,40 @@
+# Define virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnetname
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                = var.vnetname  # Name of the virtual network
+  address_space       = ["10.0.0.0/16"]  # Address space for the virtual network
+  location            = var.location  # Location of the virtual network
+  resource_group_name = var.resource_group_name  # Resource group for the virtual network
 }
 
+# Define subnet for firewall
 resource "azurerm_subnet" "firewallsubnet" {
-  name                 = var.fname
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  name                 = var.fname  # Name of the subnet
+  resource_group_name  = var.resource_group_name  # Resource group for the subnet
+  virtual_network_name = azurerm_virtual_network.vnet.name  # Name of the virtual network where the subnet is located
+  address_prefixes     = ["10.0.2.0/24"]  # Address prefix for the subnet
 }
 
+# Define public IP address
 resource "azurerm_public_ip" "EIP" {
-  name                = var.pubipname
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  name                = var.pubipname  # Name of the public IP address
+  location            = var.location  # Location of the public IP address
+  resource_group_name = var.resource_group_name  # Resource group for the public IP address
+  allocation_method   = "Static"  # Allocation method for the public IP address
+  sku                 = "Standard"  # SKU for the public IP address
 }
 
+# Define Azure Firewall
 resource "azurerm_firewall" "firewalltest" {
-  name                = var.firewall_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku_name            = var.sku_name
-  sku_tier            = var.sku_tier
+  name                = var.firewall_name  # Name of the Azure Firewall
+  location            = var.location  # Location of the Azure Firewall
+  resource_group_name = var.resource_group_name  # Resource group for the Azure Firewall
+  sku_name            = var.sku_name  # SKU name for the Azure Firewall
+  sku_tier            = var.sku_tier  # SKU tier for the Azure Firewall
 
+  # Define IP configuration for the Azure Firewall
   ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.firewallsubnet.id
-    public_ip_address_id = azurerm_public_ip.EIP.id
+    name                 = "configuration"  # Name of the IP configuration
+    subnet_id            = azurerm_subnet.firewallsubnet.id  # ID of the subnet where the Azure Firewall is deployed
+    public_ip_address_id = azurerm_public_ip.EIP.id  # ID of the public IP address associated with the Azure Firewall
   }
 }
